8ec7d4ad15d337163b2fdc4164d1b249507430e1
DaniVizel <daniella.vizelter@student.ocadu.ca>
2021-09-30 20:11:57 -0400

    webPoster

---
+++ WebPoster/WebPoster.js
@@ -0,0 +1,37 @@
+//GLOBAL VARIABLES
+var letterArray = document.querySelectorAll(".letter");
+
+
+
+
+
+// RUNNABLE CODE
+console.log("duck duck goose");
+
+// console.log(letterArray);
+// document.onclick = skew;
+
+
+document.onmousemove = moveLetters;
+
+
+
+
+
+
+// FUNCTIONS
+function moveLetters(event) {
+    var mouseX = event.clientX;
+    // console.log(mouseX);
+    for (var i=0; i< letterArray.length; i++){
+        letterArray[i].style.transform = "skew("+ 40+mouseX/10 + "deg)";
+    
+    }
+  }
+
+// function skew(){
+//     for (var i=0; i< letterArray.length; i++){
+//         letterArray[i].style.transform = "skew(40deg)";
+    
+//     }
+// }
